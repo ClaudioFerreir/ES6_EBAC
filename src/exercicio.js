a7fd679a0cbe27f1cc74aaf97d1964801c9acf3e
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-11-04 15:50:53 -0300

    fix: expression objects keys

--- src/exercicio.js
+++ src/exercicio.js
@@ -1,9 +1,9 @@
 const Matematica = [
-  {"nome": "Gabriel", "nota": 8},
-  {"nome": "Joao", "nota": 10},
-  {"nome": "Jose", "nota": 5},
-  {"nome": "Samuel", "nota": 4},
-  {"nome": "Fidel", "nota": 6}
+  {nome: "Gabriel", nota: 8},
+  {nome: "Joao", nota: 10},
+  {nome: "Jose", nota: 5},
+  {nome: "Samuel", nota: 4},
+  {nome: "Fidel", nota: 6}
 ];
 
 const aprovados = Matematica.filter(aluno => aluno.nota >= 6);
